7763bbd176926047d2704574f8f3859e5e2cfbc9
Victor <victor.boteanu@gmail.com>
2017-11-20 12:04:35 +0100

    first working version

--- madstats.py
+++ madstats.py
@@ -57,6 +57,78 @@ for lens in reversed(data):
             
 df = pd.DataFrame(data)
 
-lensmounts = ['Pentax KAF', 'Canon EF', 'Samsung NX', 'Four Thirds', 'Sony/Minolta Alpha', 'Nikon F (FX)', 'Leica M', 'Micro Four Thirds', 'Pentax KAF3', 'Sony E', 'Nikon 1', 'Fujifilm G', 'Canon EF-S', 'Nikon F (DX)', 'Sony FE', 'Leica SL', 'Canon EF-M', 'Pentax KAF2', 'Sony/Minolta Alpha DT', 'Fujifilm X', 'Leica TL', 'Pentax Q', 'Pentax 645AF2', 'Samsung NX-M']
+# too many, narrow it down to top 10 or smth
+# was 23, 24 with Leica SL
+#lensmounts = ['Pentax KAF', 'Canon EF', 'Samsung NX', 'Four Thirds', 'Sony/Minolta Alpha', 'Nikon F (FX)', 'Leica M', 'Micro Four Thirds', 'Pentax KAF3', 'Sony E', 'Nikon 1', 'Fujifilm G', 'Canon EF-S', 'Nikon F (DX)', 'Sony FE', 'Canon EF-M', 'Pentax KAF2', 'Sony/Minolta Alpha DT', 'Fujifilm X', 'Leica TL', 'Pentax Q', 'Pentax 645AF2', 'Samsung NX-M']
+# top 10:
+lensmounts = ['Canon EF','Nikon F (FX)','Sony/Minolta Alpha','Micro Four Thirds','Pentax KAF','Nikon F (DX)','Canon EF-S','Leica M','Sony E','Four Thirds']
 
-df.groupby('Lens mount')['Weight'].plot(kind='density')
+df = df[df['Lens mount'].isin(lensmounts)]
+
+#df.groupby('Lens mount')['Weight'].plot(kind='density')
+#df.groupby('Lens mount')['Weight'].plot(kind='hist', legend=True, grid=True,xlim=(0,4000))
+#df.groupby('Lens mount')['Volume'].plot(kind='density', legend=True, grid=True, xlim=(0,4000))
+# if lensmounts has fewer elements, ldf.groupby('Lens mount')['Weight'].plot(kind='density') works, so some more clean-up of NaN fields is needed I think
+
+overview = df['Lens mount'].value_counts().plot(kind='bar', legend=True, grid=True, title='Lens count by lens mount')
+for x in overview.patches:
+  overview.annotate(str(x.get_height()), (x.get_x(), x.get_height()))
+p.subplots_adjust(bottom=0.30)
+p.xticks(rotation=60)
+p.show()
+
+'''
+>>> df['Lens mount'].value_counts()
+Canon EF                 226
+Nikon F (FX)             201
+Sony/Minolta Alpha       130
+Micro Four Thirds         93
+Pentax KAF                88
+Nikon F (DX)              85
+Canon EF-S                71
+Leica M                   56
+Sony E                    53
+Four Thirds               42
+Pentax KAF3               41
+Fujifilm X                38
+Sony FE                   36
+Samsung NX                28
+Canon EF-M                17
+Pentax KAF2               15
+Nikon 1                   13
+Sony/Minolta Alpha DT     12
+Pentax Q                   8
+Fujifilm G                 6
+Pentax 645AF2              5
+Leica TL                   5
+Samsung NX-M               3
+Name: Lens mount, dtype: int64
+
+left our ridiculous crap like https://www.dpreview.com/articles/8268122124/sigma250500
+
+heaviest 20 lenses
+df[df['Weight'].notnull()].sort_values('Weight')[['Weight','Model']].tail(20)
+
+>>> df[df['Volume'].notnull()].sort_values('Volume')[['Volume','Model','Lens mount']].tail(20)
+       Volume                                    Model    Lens mount
+110    6412.0          Canon EF 500mm f/4.0L IS II USM      Canon EF
+388    6479.0             Canon EF 500mm f/4.0L IS USM      Canon EF
+605    7077.0  Nikon AF-S Nikkor 400mm f/2.8D ED-IF II  Nikon F (FX)
+215    7157.0          Canon EF 400mm f/2.8L IS II USM      Canon EF
+252    7198.0  Nikon AF-S Nikkor 400mm f/2.8E FL ED VR  Nikon F (FX)
+630    7282.0             Canon EF 400mm f/2.8L IS USM      Canon EF
+242    7399.0  Nikon AF-S Nikkor 400mm f/2.8G ED VR II  Nikon F (FX)
+149    9269.0  Nikon AF-S Nikkor 800mm f/5.6E FL ED VR  Nikon F (FX)
+140    9349.0     Nikon AF-S Nikkor 600mm F4E FL ED VR  Nikon F (FX)
+286    9620.0             Canon EF 800mm f/5.6L IS USM      Canon EF
+60     9631.0       Nikon AF-S Nikkor 600mm f/4G ED VR  Nikon F (FX)
+588    9847.0    Nikon AF-S Nikkor 600mm f/4D ED-IF II  Nikon F (FX)
+540    9953.0          Canon EF 600mm f/4.0L IS II USM      Canon EF
+1278  10086.0               Sigma 800mm F5.6 EX DG HSM      Canon EF
+1279  10086.0               Sigma 800mm F5.6 EX DG HSM  Nikon F (FX)
+646   10108.0             Canon EF 600mm f/4.0L IS USM      Canon EF
+1292  10531.0           Sigma 300-800mm F5.6 EX DG HSM  Nikon F (FX)
+1291  10531.0           Sigma 300-800mm F5.6 EX DG HSM      Canon EF
+1145  32027.0               Sigma 200-500mm F2.8 EX DG      Canon EF
+1146  32027.0               Sigma 200-500mm F2.8 EX DG  Nikon F (FX)
+'''
